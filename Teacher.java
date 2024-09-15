7c50c1605fb00fd2d49f5ed2a465fab79fdc1ed4
Marina Barykina <marinakliman9@gmail.com>
2024-09-15 18:21:52 +1000

    класс учитель

---
+++ Teacher.java
@@ -0,0 +1,36 @@
+public class Teacher {
+    private int id;
+    private String name;
+    private String subject;
+
+    public Teacher(int id, String name, String subject) {
+        this.id = id;
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher [ID: " + id + ", Name: " + name + ", Subject: " + subject + "]";
+    }
+}
